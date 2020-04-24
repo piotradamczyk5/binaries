fbb1d89eed24a3a411a0d257b5ab1bda5960c426
bootstraponline <code@bootstraponline.com>
2020-04-23 21:44:13 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -26,8 +26,9 @@ fi
 # http://releases.llvm.org/download.html
 
 LLVM="llvm/llvm.tar.xz"
-if [ ! -f $LLVM ]; then
+if [ ! -f "llvm/llvm.tar" ]; then
   mkdir -p llvm
+  echo "Downloading llvm"
   curl -L https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz -o $LLVM
   pushd llvm
   xz -d llvm.tar.xz
@@ -46,6 +47,7 @@ fi
 SWIFT="swift/swift.tar.gz"
 if [ ! -f $SWIFT ]; then
   mkdir -p swift
+  echo "Downloading swift"
   curl -L https://swift.org/builds/swift-5.2.2-release/ubuntu1804/swift-5.2.2-RELEASE/swift-5.2.2-RELEASE-ubuntu18.04.tar.gz -o $SWIFT
   pushd swift
   tar -xzf swift.tar.gz
